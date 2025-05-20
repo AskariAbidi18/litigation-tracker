4060d13eabac9228c8478c1ad3bb9b687061445b
AskariAbidi18 <askari.abidi.2005@gmail.com>
2025-05-20 16:01:48 +0530

    litigation-tracker

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+// firebase_config.js
+
+// Export Firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyCHwxHAxWSNOkEqCPWTUQ-8n1YA-8kcrI0",
+  authDomain: "litigation-tracker-55f64.firebaseapp.com",
+  projectId: "litigation-tracker-55f64",
+  storageBucket: "litigation-tracker-55f64.appspot.com",
+  messagingSenderId: "937375454085",
+  appId: "1:937375454085:web:46de7372ef8d3d888deaec",
+  measurementId: "G-P9SWJPNE5F"
+};
